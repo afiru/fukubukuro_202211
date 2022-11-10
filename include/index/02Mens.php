ddb0ddbb8123851e3bfcfda6b665d48541327feb
Naoki Soga <n.soga@sunplacom>
2022-11-10 15:49:55 +0900

    作成完了

--- include/index/02Mens.php
+++ include/index/02Mens.php
@@ -1,19 +1,24 @@
-<section class="bgE0CBAF tabMainLxc">
+<section class="bgMainLxcBoy tabMainLxc">
     <h1 class="tabMainLxcH1">
-        <img loading="lazy" src="img/tabMainLxcH1Mens.svg" alt="福袋メンズラインナップ" width="369" height="115" />
+        <img loading="lazy" src="img/tabMainLxcH1Mens.svg" alt="福袋メンズラインナップ" width="352.26" height="198.4" />
     </h1>
     <ul class="tabMainLxcUl">
         <li class="tabMainLxcLi tabMainLxcLi01">
-            <img loading="lazy" src="img/tabMainLxcLiMens01.webp" alt="KRIFF MAYERはベーシックでありながらオリジナリティあふれるスタイルで、新しいアメリカンデイリーウェアとして、ライフスタイルを提案しています。今年の福袋は秋・冬・春とロングシーズン使える内容になっています。　11,000円" width="640" height="1072" />
+            <img loading="lazy" src="img/tabMainLxcLiMens01.webp" alt="" width="640" height="1072" />
         </li>
         <li class="tabMainLxcLi tabMainLxcLi02">
-            <img loading="lazy" src="img/tabMainLxcLiMens02.webp" alt="“サーフ・ハワイアンカルチャー×トレンドの融合”をテーマに、お洒落な大人の遊び心を刺激するカジュアルブランドANTIBALLISTICの福袋。毎年完売必須の大人気ブランドです。 11,000円" width="640" height="1072" />
+            <img loading="lazy" src="img/tabMainLxcLiMens02.webp" alt="" width="640" height="1072" />
         </li>
         <li class="tabMainLxcLi tabMainLxcLi03">
-            <img loading="lazy" src="img/tabMainLxcLiMens03.webp" alt="昨年大好評を頂いたあの福袋が今年も登場！BettySmithの定番商品の中から、選りすぐりの商品が3点入っております。サイズ別だから履けなかったという心配も少なく、お得感のある内容になっています。　11,000円" width="640" height="1072" />
-        </li>
-        <li class="tabMainLxcLi tabMainLxcLi04">
-            <img loading="lazy" src="img/tabMainLxcLiMens04.webp" alt="11,000円" width="640" height="1072" />
+            <img loading="lazy" src="img/tabMainLxcLiMens03.webp" alt="" width="640" height="1072" />
         </li>
     </ul>
+    <p class="txtMainLxcBoy">
+        <img loading="lazy" src="img/txtMainBoyLxc.svg" alt="福袋メンズラインナップ" width="624.56" height="101.26" />
+    </p>
+    <div class="btmMainBoyWap">
+        <a class="btmMainBoy" href="https://www.geography.co.jp/shop/" target="_blank">
+            <img loading="lazy" src="img/btmMainBoy.svg" alt="福袋メンズラインナップ" width="624.56" height="101.26" />
+        </a>
+    </div>
 </section>
